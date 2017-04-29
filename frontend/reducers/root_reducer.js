c43d8c3e387bc9e42199df92de37bb9e813e5676
Daniel Woo <dwoo@prosper.com>
2017-04-29 16:47:29 -0700

    add lodash, and example reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 // import sub-reducers here
+import examples from './example_reducer';
 
 const rootReducer = combineReducers({
-
+  examples,
 });
 
 export default rootReducer;
